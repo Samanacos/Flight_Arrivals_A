ef0e6f88b0b35b5d491bba5876476f606b10fe5c
Giannis Terzakis <59915445+Samanacos@users.noreply.github.com>
2022-09-15 20:26:12 +0300

    Add files via upload

---
+++ FlightArrivals A/Flight.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FlightArrivals_A
+{
+    public class Flight
+    {
+        public string FlightNo;
+        public string DepartName;
+        public string ArrivalTime;
+
+        public Flight(string flNo, string dep, string time)
+        {
+            FlightNo = flNo;
+            DepartName = dep;
+            ArrivalTime = time;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0,-15}{1,-15}{2,-10}", FlightNo, DepartName, ArrivalTime);
+        }
+    }
+}
